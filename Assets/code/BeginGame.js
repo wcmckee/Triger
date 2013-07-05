3fbad3ad7234480027fdc8c3e17991a3058ff979
artctrl <will@artcontrol.me>
2013-07-06 03:38:53 +1200

    Renamed scripts Fixed levels selection
    Signed-off-by: artctrl <will@artcontrol.me>

--- Assets/code/BeginGame.js
+++ Assets/code/BeginGame.js
@@ -1,10 +1,10 @@
 function OnGUI () {
 
 	// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-	if (GUI.Button (Rect (300,250,156,56), "Load Memory")) {
+	if (GUI.Button (Rect (300,250,156,56), "Controls")) {
 		Application.LoadLevel (8);
 	}
-	if (GUI.Button (Rect (300,306,156,56), "Remove RAM")) {
-		Application.LoadLevel (9);
+	if (GUI.Button (Rect (300,306,156,56), "Return to current time")) {
+		Application.LoadLevel (0);
 	}
 }
