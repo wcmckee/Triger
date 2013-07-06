f7993eee656eff9f94898d85b99af67f56f46f2e
lvnhk <will@artcontrol.me>
2013-07-07 04:00:55 +1200

    created a bunch of levels

--- Assets/code/LoadLevel.js
+++ Assets/code/LoadLevel.js
@@ -1,6 +1,6 @@
 function OnGUI () {
 	// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
 	if (GUI.Button (Rect (150,200,156,56), "Trigger Zero")) {
-		Application.LoadLevel(Random.Range(0, 9));
+		Application.LoadLevel(Random.Range(0, 13));
 	}
 }
