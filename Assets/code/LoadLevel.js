6b6820224743d5b458ebe25bb05bc576cee7caa0
lvnhk <will@artcontrol.me>
2013-07-07 03:35:45 +1200

    created new first person level

--- Assets/code/LoadLevel.js
+++ Assets/code/LoadLevel.js
@@ -1,15 +1,6 @@
 function OnGUI () {
 	// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
 	if (GUI.Button (Rect (150,200,156,56), "Trigger Zero")) {
-		Application.LoadLevel (0);
-	}
-
-	// Make the second button.
-	if (GUI.Button (Rect (150,256,156,56), "Trigger One")) {
-		Application.LoadLevel (1);
-	}
-		
-	if(GUI.Button (Rect (150,312,156,56), "Trigger Two")) {
-		Application.LoadLevel (2);
+		Application.LoadLevel(Random.Range(0, 9));
 	}
 }
